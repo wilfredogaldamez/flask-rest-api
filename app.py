6b6fab6041de943fd5e5a855b0a8b3af05a09219
Wilfredo Galdamez <wilfredogaldamez1994@gmail.com>
2021-01-25 13:48:21 -0500

    added rest api

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return jsonify({"about": "hello, world!"})
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        some_json = request.get_json()
+        return jsonify({'you sent': some_json}), 201
+    else:
+        return jsonify({"about": "hello, world!"})
+
+@app.route("/multi/<int:num>", methods=['GET'])
+def get_multiply10(num):
+    return jsonify({'result': num*10})
 
 if __name__ == '__main__':
     app.run(debug=True)
